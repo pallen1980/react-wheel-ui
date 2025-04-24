7a57946d97aaee6c6171f092606a0867746c20cb
Phil Allen <69210558+pallen1980@users.noreply.github.com>
2025-04-24 23:21:02 +0100

    Added Firebase OAuth (#3)
    * updated main.yml to include permissions for better security
    
    * Add Oauth
    
    * updated readme
    
    * removed package-lock.json and ensured it doesnt get in the way of further CI
    
    * try to stop git from uploading package-lock.json
    
    * try again
    
    * and again
    
    * and again
    
    * updated readme
    
    * firebase auth now working in docker container

--- src/Auth/Firebase/Config/Firebase.tsx
+++ src/Auth/Firebase/Config/Firebase.tsx
@@ -2,15 +2,28 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 
+declare global {
+  interface Window {
+    FIREBASE_API_KEY?: string;
+    FIREBASE_AUTH_DOMAIN?: string;
+    FIREBASE_DATABASE_URL?: string;
+    FIREBASE_PROJECT_ID?: string;
+    FIREBASE_STORAGE_BUCKET?: string;
+    FIREBASE_MESSAGING_SENDER_ID?: string;
+    FIREBASE_APP_ID?: string;
+    FIREBASE_MEASUREMENT_ID?: string;
+  }
+}
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ?? window.FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ?? window.FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL ?? window.FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ?? window.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ?? window.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ?? window.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID ?? window.FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ?? window.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
